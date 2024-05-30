7928d3b0f19aa0e9055a3312b11b8f932891cd07
puanabila <puannabila1969@gmail.com>
2024-05-30 19:25:37 +0700

    Initial commit

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'User',
+                'email' => 'user@email.com',
+                'type' => 0,
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@email.com',
+                'type' => 1,
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Manager User',
+                'email' => 'manager@email.com',
+                'type' => 2,
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Super Admin',
+                'email' => 'superadmin@email.com',
+                'type' => 3,
+                'password' => bcrypt('123456'),
+            ]
+        ];
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
